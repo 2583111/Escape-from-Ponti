663795dd69249ce08a98d6a520564f205ab5b0c9
Sinethemba <2553293@students.wits.ac.za>
2023-10-31 21:11:13 +0200

    SoundsandLighting
    Sounds and Lighting_

---
+++ Assets/Scenes/SNE-Scenes and stuff/Scripts/Doors.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    public Animator door;
+    public GameObject openText;
+
+    public bool inReach;
+
+    void Start()
+    {
+        inReach = false;
+        openText.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            openText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            openText.SetActive(false);
+        }
+    }
+
+
+
+
+
+    void Update()
+    {
+
+        if (inReach)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (door.GetBool("Closed"))
+                {
+                    DoorOpens();
+                    openText.SetActive(false);
+                }
+            }
+        else
+        {
+            openText.SetActive(true);
+            }
+        }
+        else
+        {
+            openText.SetActive(false);
+        }
+        
+
+
+
+
+
+    }
+    void DoorOpens ()
+    {
+        Debug.Log("It Opens");
+        door.SetBool("Open", true);
+        door.SetBool("Closed", false);
+       
+
+    }
+
+    void DoorCloses()
+    {
+        Debug.Log("It Closes");
+        door.SetBool("Open", false);
+        door.SetBool("Closed", true);
+    }
+
+
+}
