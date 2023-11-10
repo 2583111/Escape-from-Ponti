136c56805c7465930e44bc9531523e4fcff78f5b
cameronsmorgan <131270279+cameronsmorgan@users.noreply.github.com>
2023-11-10 17:34:07 +0200

    level Update

--- Assets/Scripts/Player/Health.cs
+++ Assets/Scripts/Player/Health.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Health : MonoBehaviour
-{
-    public Slider healthBar;
+//public class Health : MonoBehaviour
+//{
+   // public Slider healthBar;
 
-    public void SetMaxHealth(int health)
-    {
-        healthBar.maxValue = health;
-        healthBar.value = health;
-    }
+   // public void SetMaxHealth(int health)
+  //  {
+        //healthBar.maxValue = health;
+        //healthBar.value = health;
+  //  }
 
-    public void SetHealthBar(int health)
-    {
-        healthBar.value = health;
-    }
-}
+    //public void SetHealthBar(int health)
+   // {
+      //  healthBar.value = health;
+    //}
+//}
