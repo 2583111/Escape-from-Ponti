2ebd14b597342a2376bbb43471cca06409a305a8
cameronsmorgan <131270279+cameronsmorgan@users.noreply.github.com>
2023-11-13 09:53:42 +0200

    level update

--- Assets/Elevator.cs
+++ Assets/Elevator.cs
@@ -6,10 +6,26 @@ public class Elevator : MonoBehaviour
 {
     public GameObject movePlatform;
     public float elevatorSpeed = 1.0f; // Adjust this value to control the elevator speed.
+    public float delayTime = 2.0f; // Adjust this value to set the delay before the elevator starts moving.
+
+    private bool canMove = false;
 
     private void OnTriggerStay(Collider other)
     {
-        // Move the platform by multiplying the up vector with the elevator speed and Time.deltaTime.
-        movePlatform.transform.position += movePlatform.transform.up * elevatorSpeed * Time.deltaTime;
+        if (!canMove)
+        {
+            StartCoroutine(StartElevatorAfterDelay());
+        }
+        else
+        {
+            // Move the platform by multiplying the up vector with the elevator speed and Time.deltaTime.
+            movePlatform.transform.position += movePlatform.transform.up * elevatorSpeed * Time.deltaTime;
+        }
+    }
+
+    IEnumerator StartElevatorAfterDelay()
+    {
+        yield return new WaitForSeconds(delayTime);
+        canMove = true;
     }
 }
